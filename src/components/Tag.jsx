4ced208663e67bbc66af245f25094d0f285dc866
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-06-06 23:56:29 +0530

    Tag , selectTag and buttons style

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Tag.css'
 
-const Tag = (props) => {
+const Tag = ({tagName, selectTag, selected}) => {
+  const tagStyle = {
+    HTML : {
+      backgroundColor: '#e34c26',
+      color: 'white'
+    },
+    CSS : {
+      backgroundColor: '#264de4',
+      color: 'white'
+    },
+    JavaScript : {
+      backgroundColor: '#f7df1e',
+      color: 'black'
+    },
+    React : {
+      backgroundColor: '#61dafb',
+      color: 'black'
+    },
+    default: {
+      backgroundColor: '#ccc',
+      color: 'black'
+    }
+  }
   return (
-            <button className='tag'>{props.tagName}</button>
+            <button  type='button' 
+            className='tag'
+            style={selected ? tagStyle[tagName] : tagStyle.default} 
+            onClick={() => selectTag(tagName)}>
+            {tagName}
+            </button>
   )
 }
 
