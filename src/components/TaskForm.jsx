5715af467ca9825312f3506106f1ae9760029823
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-06-06 23:50:12 +0530

    TaskForm checkTag and selectTag

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,86 @@
+import React ,{useState} from 'react'
+import './TaskForm.css'
+import Tag from './Tag.jsx'
+
+const TaskForm = () => {
+  const [taskData, setTaskData] = useState({
+    task : '',
+    status: 'todo',
+    tag: []
+  });
+
+  const checkTag = (tag) => {
+    return taskData.tag.some(item => item === tag);
+  }
+
+  const selectTag = (tag) => {
+    if (taskData.tag.some(item => item === tag)) {
+      const filteredTags = taskData.tag.filter(item => item !== tag);
+      setTaskData((prevData) => ({
+        ...prevData,
+        tag: filteredTags,
+      }));
+  }else {
+    setTaskData((prevData) => ({
+      ...prevData,
+      tag: [...prevData.tag, tag],
+    }));
+  }
+}
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Task submitted:', taskData);
+  }
+  // const [task, setTask] = useState('');
+  // const[status, setStatus] = useState('todo');
+  // const handleTaskChange = (e) => {
+  //   setTask(e.target.value);
+  // };
+  // const handleStatusChange = (e) => {
+  //   setStatus(e.target.value);
+  // };
+  return (
+   <header className='app_header'>
+    <form onSubmit={handleSubmit} className='task_form'>
+      <input type="text" 
+             name='task'
+             className='task_input'
+             placeholder='Enter your task'
+             onChange={handleChange}
+      />
+      <div className='task_form_bottom_line'>
+        <div>
+         <Tag tagName="HTML" selectTag = {selectTag} selected={checkTag("HTML")}/>
+            <Tag tagName="CSS" selectTag = {selectTag} selected={checkTag("CSS")}/>
+            <Tag tagName="JavaScript"selectTag = {selectTag} selected={checkTag("JavaScript")}/>
+            <Tag tagName="React" selectTag = {selectTag} selected={checkTag("React")}/>
+
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <select 
+                  name='status'
+                  className='task_status'
+                  onChange={handleChange}
+          >
+            <option value="todo">To do</option>
+            <option value="in-progress">In Progress</option>
+            <option value="done">Done</option>
+          </select>
+          <button type='submit' className='task_submit'>Add Task</button>
+        </div>
+      </div>
+    </form>
+   </header>
+  )
+}
+
+export default TaskForm
