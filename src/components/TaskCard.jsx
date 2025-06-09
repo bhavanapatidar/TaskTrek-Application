05a1e379ec2f6093f1be14c216f6d67c3d9b8c78
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-06-09 23:51:26 +0530

    delete functionality

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -4,9 +4,9 @@ import './Tag.jsx'
 import Tag from './Tag.jsx'
 import deleteIcon from '../assets/delete.png'
 
-const TaskCard = ({taskColumnName, tags = [], handleDelete, index}) => {
+const TaskCard = ({taskColumnName, tags = [], handleDelete, index,setActiveCard}) => {
   return (
-    <article className='task-card'>
+    <article className='task-card' draggable onDragStart={()=> setActiveCard(index)} onDragEnd={() => setActiveCard(null)}>
         <p className='task_text'> {taskColumnName}</p>
         <div className='task_card_bottom_line'>
             <div className='task_card_tags'>
