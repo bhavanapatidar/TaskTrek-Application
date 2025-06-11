b2268781986c4d2971c6258a6ce00e24bb60d0bb
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-06-11 18:53:04 +0530

    Adding Drag and drop feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import TaskForm from './components/TaskForm'
 import TaskColumn from './components/TaskColumn.jsx'
@@ -6,16 +6,41 @@ import todoIcon from './assets/direct-hit.png'
 import inprogresIcon from './assets/glowing-star.png'
 import doneIcon from './assets/check-mark-button.png'
 
+const oldTasks = localStorage.getItem("tasks")
+console.log(oldTasks);
+
 const App = () => {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState(JSON.parse(oldTasks)||[]);
+  const [activeCard, setActiveCard] = useState(null);
+  useEffect(() => {
+   localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+
+
+  const handleDelete = (taskIndex) => {
+   const newTasks = tasks.filter((task,index) => index !== taskIndex);
+    setTasks(newTasks);
+  }
+
+  const onDrop = (status, position) => {
+    if (activeCard == null || activeCard === undefined ) return;
+    const taskToMove = tasks[activeCard];
+    const updatedTasks = tasks.filter((task, index) => index !== activeCard);
+    updatedTasks.splice(position, 0, {
+      ...taskToMove,
+      status: status
+    });
+    setTasks(updatedTasks);
+  }
   return (
     <div className='app'>
       <TaskForm setTasks={setTasks}/>
       <main className='app_main'>
-        <TaskColumn taskColumnName = "To Do" icon = {todoIcon} tasks = {tasks} status = "todo"/>
-        <TaskColumn taskColumnName = "In Progress" icon = {inprogresIcon} tasks = {tasks} status = "in-progress"/> 
-        <TaskColumn taskColumnName = "Done" icon = {doneIcon} tasks = {tasks} status = "done"/>    
+        <TaskColumn title = "To Do" icon = {todoIcon} tasks = {tasks} status = "todo" handleDelete = {handleDelete} setActiveCard = {setActiveCard} onDrop= {onDrop}/>
+        <TaskColumn title = "In Progress" icon = {inprogresIcon} tasks = {tasks} status = "in-progress" handleDelete = {handleDelete} setActiveCard = {setActiveCard} onDrop = {onDrop}/> 
+        <TaskColumn title = "Done" icon = {doneIcon} tasks = {tasks} status = "done" handleDelete = {handleDelete} setActiveCard = {setActiveCard} onDrop = {onDrop}/>    
       </main>
+      
     </div>
   )
 }
