bcc51bc900f6ffc91a47cddd30d89ceed06076bd
bhavanapatidar <bhavanapatidar2708@gmail.com>
2025-06-11 18:55:18 +0530

    Adding Drag and drop feature

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -2,25 +2,32 @@ import React from 'react'
 import Todo from '../assets/direct-hit.png'
 import './TaskColumn.css'
 import TaskCard from './TaskCard'
+import DropArea from './DropArea'
 
-const TaskColumn = ({icon,taskColumnName, tasks, status, handleDelete, setActiveCard}) => {
+const TaskColumn = ({icon,taskColumnName,title, tasks, status, handleDelete, setActiveCard, onDrop}) => {
   return (
        <section className='task_column'>
         <h2 className='task_column_heading'>
             <img className='task_column_icon' src={icon} 
-            alt=''/>{taskColumnName}</h2>
+            alt=''/>{title}</h2>
+           <DropArea onDrop={() => onDrop(status, 0)}/>
+
+
 
             {
                tasks.map(
                     (task, index) => 
                task.status === status && (
-               <TaskCard key={index}
-                taskColumnName={task.task} 
+                    <React.Fragment key={index}>
+               <TaskCard 
+                title={task.task} 
                tags={task.tags}
                handleDelete = {handleDelete}
                index={index}
                setActiveCard={setActiveCard}
                />
+               <DropArea onDrop={() => onDrop(status, index+1)}/>
+               </React.Fragment>
           )
           )
             }
